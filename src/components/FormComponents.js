7973609b697ad953704638e1fd844ce79bc48c8e
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-27 15:38:31 -0400

    create components

---
+++ src/components/FormComponents.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class FormComponents extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return()
+    }
+}
---
+++ src/components/ListComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ListComponents extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <img src=" " alt="logo" />
+                <ul>
+                    
+                </ul>
+            </div>
+        )
+    }
+}
