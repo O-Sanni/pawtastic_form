53bb67499b2aa35dd60b4402f4749223c4a5bda5
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-30 17:56:12 -0400

    finished working page

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -11,12 +11,14 @@ class ListComponent extends React.Component{
             <div id="leftSide">
                 <div id="logoTxt">
                     <img id="logo" src={require("../assets/logo.png")} alt="logo" />
-                    <ul>
-                        <li>Human Profile</li>
-                        <li>Pet basics</li>
-                        <li>Pet details</li>
-                        <li>Confirm</li>
-                    </ul>
+                    <div id="line">
+                        <ul>
+                            <li>Human Profile</li>
+                            <li>Pet basics</li>
+                            <li>Pet details</li>
+                            <li>Confirm</li>
+                        </ul>
+                    </div>
                     <p id="save">Save and exit</p>
                 </div>
             </div>
