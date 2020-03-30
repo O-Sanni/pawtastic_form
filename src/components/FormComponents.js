eaf81029064fbe501e67a55440e8768e80bb3303
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-30 00:07:18 -0400

    Working choices

--- src/components/FormComponents.js
+++ src/components/FormComponents.js
@@ -32,24 +32,36 @@ class FormComponents extends React.Component{
                         <p>Breed</p>
                         <input type="text" onChange={this.handleChangeBreed} placeholder="Pet's breed" />
                         </form>
-                        <div id="Gender">
+                        <div id="gender">
                             <p>Gender</p>
                             <div id="genderChoice">
-                            <input className="choiceInp" id="female" type="radio" name="choiceGender" checked/>
+                            <input className="choiceInp" id="female" type="radio" name="choiceGender" />
                             <label id="femaleChoice" for="female">Female</label>
                             <input className="choiceInp" id="male" type="radio" name="choiceGender"/>
                             <label id="maleChoice" for="male">Male</label>
                             </div>
-                            <div id="Gender">
+                            <div id="spaNeu">
                             <p>Spayed or Neutered</p>
-                            <div id="SpaOrNeu">
-                            <input className="choiceInp" id="yes" type="radio" name="yesNo" checked/>
+                            <div id="spaOrNeu">
+                            <input className="choiceInp" id="yes" type="radio" name="yesNo" />
                             <label id="yesChoice" for="yes">Yes</label>
                             <input className="choiceInp" id="no" type="radio" name="yesNo"/>
                             <label id="noChoice" for="no">No</label>
                             </div>
                             </div>
-                
+                            <div id="weight">
+                            <p>Weight</p>
+                            <div id="weightCh">
+                            <input className="choiceInp" id="up25lbs" type="radio" name="weight" />
+                            <label id="choiceUp25" for="up25lbs">0-25lbs</label>
+                            <input className="choiceInp" id="up50lbs" type="radio" name="weight"/>
+                            <label id="choiceUp50" for="up50lbs">25-50lbs</label>
+                            <input className="choiceInp" id="up100lb" type="radio" name="weight"/>
+                            <label id="choiceUp100" for="up100lb">50-100lbs</label>
+                            <input className="choiceInp" id="more100lb" type="radio" name="weight"/>
+                            <label id="choiceMore100" for="more100lb">100+lbs</label>
+                            </div>
+                            </div>
                 
                       
             </div>
