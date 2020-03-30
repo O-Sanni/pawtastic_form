d85977f8ee9bc9e18de955af3a671c9980ba3971
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-29 23:08:42 -0400

    Working choice gender

--- src/components/FormComponents.js
+++ src/components/FormComponents.js
@@ -10,12 +10,17 @@ class FormComponents extends React.Component{
             birth: " "
         }
         this.handleChangeName=this.handleChangeName.bind(this);
+        this.handleChangeBreed=this.handleChangeBreed.bind(this);
     }
     handleChangeName(e){
         this.setState({name:e.target.value});
+        e.preventDefault();
         
     }
-    handleChangeBreed
+    handleChangeBreed(e){
+        this.setState({breed:e.targed.value});
+        e.preventDefault();
+    }
     render(){
         return(
             <div id="rightMain">
@@ -24,19 +29,23 @@ class FormComponents extends React.Component{
                     <form id="formTop">
                         <p>Name</p>
                         <input type="text" onChange={this.handleChangeName} placeholder="Pet's name" />
-{/*                         
                         <p>Breed</p>
-                        <input type="text" breed ={this.state.breed} placeholder="Pet's breed" />
+                        <input type="text" onChange={this.handleChangeBreed} placeholder="Pet's breed" />
                         <div>
-                        <div>
+                        <div id="Gender">
                             <p>Gender</p>
-                            <input name="Female"/>
-                        </div>
-                        
-                        </div> */}
-                    </form>
-                </div>
+                            <input id="female" type="radio" name="choiceGender" checked/>
+                            <label id="femaleChoice" for="female">Female</label>
+                            <input id="male" type="radio" name="choiceGender"/>
+                            <label id="maleChoice" for="male">Male</label>
+                      
+               
+  
+</div>
+                            </div>
+                </form>
                 <div id="formButton"></div>
+            </div>
             </div>
         )
     }
