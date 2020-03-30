79145a53abb1709ccbf1e45de8804d1add70bad1
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-30 17:47:35 -0400

    finished working page

--- src/components/FormComponents.js
+++ src/components/FormComponents.js
@@ -5,9 +5,12 @@ class FormComponents extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            name: " ",
-            breed: " ",
-            birth: " ",
+            name: "",
+            breed: "",
+            birth: "",
+            gender:"",
+            yOrN: "",
+            weight: "",
             back: false,
             next: false
         }
@@ -16,36 +19,52 @@ class FormComponents extends React.Component{
         this.handleChangeBirthday=this.handleChangeBirthday.bind(this);
         this.handlePressBack=this.handlePressBack.bind(this);
         this.handlePressNext=this.handlePressNext.bind(this);
+        this.chooseGender=this.chooseGender.bind(this);
+        this.chooseYorN=this.chooseYorN.bind(this);
+        this.chooseWeight=this.chooseWeight.bind(this);
+    
     }
 
     handleChangeName(e){
         this.setState({name:e.target.value});
-        e.preventDefault();
-        
+        e.preventDefault();      
     }
 
     handleChangeBreed(e){
-        this.setState({breed:e.targed.value});
+        this.setState({breed:e.target.value});
         e.preventDefault();
     }
 
     handleChangeBirthday(e){
-        this.setState({breed:e.targed.value});
+        this.setState({birth:e.target.value});
+        e.preventDefault();
+    }
+    chooseGender(e){
+        this.setState({gender:e.target.value});
+        e.preventDefault();
+    }
+    chooseYorN(e){
+        this.setState({yOrN:e.target.value});
+        console.log(this.state.yOrN);
+        e.preventDefault();
+    }
+    chooseWeight(e){
+        this.setState({weight:e.target.value});
         e.preventDefault();
     }
 
     handlePressBack(e){
         this.setState({back:true});
+        e.preventDefault();
         document.getElementById("next").style.backgroundColor="#ffffff";
         document.getElementById("next").style.color="#545871";
         document.getElementById("back").style.backgroundColor="#545871";
         document.getElementById("back").style.color="#ffffff";
-        console.log("Back: " + this.state.back);
     }
 
     handlePressNext(e){
         this.setState({next:true})
-        console.log("Next: " + this.state.next);
+        e.preventDefault();
         document.getElementById("back").style.backgroundColor="#ffffff";
         document.getElementById("back").style.color="#545871";
         document.getElementById("next").style.backgroundColor="#545871";
@@ -80,33 +99,33 @@ class FormComponents extends React.Component{
                             <div id="genderSN">
                                 <div id="gender">
                                     <p className="pNames">Gender</p>
-                                    <form id="genderChoice">
-                                        <input className="choiceInp" id="female" type="radio" name="choiceGender" />
+                                    <form id="genderChoice" onChange={this.chooseGender}>
+                                        <input className="choiceInp" id="female" value="Female" type="radio" name="choiceGender" />
                                         <label className="labelsRadio" id="femaleChoice" for="female">Female</label>
-                                        <input className="choiceInp" id="male" type="radio" name="choiceGender"/>
+                                        <input className="choiceInp" id="male" value="Male" type="radio" name="choiceGender"/>
                                         <label className="labelsRadio" id="maleChoice" for="male">Male</label>
                                     </form>
                                 </div>
                                 <div id="spaNeu">
                                     <p className="pNames">Spayed or Neutered</p>
-                                    <form id="spaOrNeu">
-                                        <input className="choiceInp" id="yes" type="radio" name="yesNo" />
+                                    <form id="spaOrNeu" onChange={this.chooseYorN}>
+                                        <input className="choiceInp" id="yes" value="Yes" type="radio" name="yesNo" />
                                         <label className="labelsRadio" id="yesChoice" for="yes">Yes</label>
-                                        <input className="choiceInp" id="no" type="radio" name="yesNo"/>
+                                        <input className="choiceInp" id="no"  value="No" type="radio" name="yesNo"/>
                                         <label className="labelsRadio" id="noChoice" for="no">No</label>
                                     </form>
                                 </div>   
                             </div>
                             <div id="weight">
                                 <p className="pNames">Weight</p>
-                                <form id="weightCh">
-                                    <input className="choiceInp"  id="up25lbs" type="radio" name="weight" />
+                                <form id="weightCh" onChange={this.chooseWeight}>
+                                    <input className="choiceInp"  id="up25lbs" value="0-25lbs" type="radio" name="weight" />
                                     <label className="radioWeight" id="choiceUp25" for="up25lbs">0-25lbs</label>
-                                    <input className="choiceInp" id="up50lbs" type="radio" name="weight"/>
+                                    <input className="choiceInp" id="up50lbs"  value="25-50lbs" type="radio" name="weight"/>
                                     <label className="radioWeight" id="choiceUp50" for="up50lbs">25-50lbs</label>
-                                    <input className="choiceInp" id="up100lb" type="radio" name="weight"/>
+                                    <input className="choiceInp" id="up100lb" value="50-100lbs" type="radio" name="weight"/>
                                     <label className="radioWeight" id="choiceUp100" for="up100lb">50-100lbs</label>
-                                    <input className="choiceInp" id="more100lb" type="radio" name="weight"/>
+                                    <input className="choiceInp" id="more100lb" value="100+lbs"type="radio" name="weight"/>
                                     <label className="radioWeight" id="choiceMore100" for="more100lb">100+lbs</label>
                                 </form>
                             </div>
